d2510fc9c9192c512854a0bf3e4c93f2698f5d54
Hyoniii <phhphh841@hanmail.net>
2020-08-23 10:50:56 +0900

    게시판 카테고리 생성

---
+++ card.js
@@ -0,0 +1,16 @@
+const ROW = 4;
+const COLUMN = 3;
+const colors = [
+  "red",
+  "red",
+  "orange",
+  "orange",
+  "green",
+  "green",
+  "yellow",
+  "yellow",
+  "white",
+  "white",
+  "pink",
+  "pink",
+];
